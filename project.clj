9d1251d8f97478451b4fa29419ee49e3d6166ed3
Duncan McGreggor <oubiwann@gmail.com>
2018-08-29 14:46:28 -0500

    Added build aliases.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojusc/osgi "1.9.0-3-SNAPSHOT"
+(defproject clojusc/osgi "1.9.0.3"
   :description "Clojure - OSGi-ified with support library"
   :url "https://github.com/talios/clojure.osgi"
   :license {:name "Eclipse Public License"}
@@ -10,7 +10,47 @@
     [org.clojure/spec.alpha "0.1.143"]
     [farana "0.1.0-SNAPSHOT"]]
   :plugins [
+    [lein-shell "0.5.0"]
     [org.apache.felix/maven-bundle-plugin "3.5.1"]
     [org.apache.maven.plugins/maven-compiler-plugin "3.7.0"
-    [org.apache.maven.plugins/maven-source-plugin "3.0.1"]
-    [org.apache.maven.plugins/maven-release-plugin "2.5.3"]]])
+    [org.apache.maven.plugins/maven-release-plugin "2.5.3"]
+    [org.apache.maven.plugins/maven-source-plugin "3.0.1"]]]
+  :aliases {
+    "clean" ["do"
+      ["shell" "echo"]
+      ["shell" "echo" "Cleaning build ..."]
+      ["shell" "echo"]
+      ["shell" "mvn" "clean:clean"]]
+    "compile" ["do"
+      ["shell" "echo"]
+      ["shell" "echo" "Compiling clojure.osgi ..."]
+      ["shell" "echo"]
+      ["shell" "mvn" "compile"]]
+    "bundle" ["do"
+      ["shell" "echo"]
+      ["shell" "echo" "Packaging clojure.osgi bundle ..."]
+      ["shell" "echo"]
+      ["shell" "mvn" "bundle:bundle"]]
+    "build" ["do"
+      ["clean"]
+      ["compile"]
+      ["bundle"]]
+    "local" ["do"
+      ["shell" "echo"]
+      ["shell" "echo" "Deploying clojure.osgi locally ..."]
+      ["shell" "echo"]
+      ["shell" "mvn" "install"]]
+    "clojars" ["do"
+      ["shell" "echo"]
+      ["shell" "echo"
+       "Publishing"
+       :project/group
+       :project/name
+       :project/version
+       "to Clojars ..."]
+      ["shell" "echo"]
+      ["shell" "./clojars-deploy"
+       :project/group
+       :project/name
+       :project/version
+       :project/target-path]]})
